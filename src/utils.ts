19250b4421e62fe76573d60cc92ebe8ad159f5f4
Fernando del olmo <fer.olmo92@gmail.com>
2018-01-31 18:06:10 +0100

    Added feat POST url

--- src/utils.ts
+++ src/utils.ts
@@ -7,7 +7,7 @@ export const GET_FEATURES = 'https://us-central1-ioniczen-7709c.cloudfunctions.n
 export const GET_FLOWS = 'https://us-central1-ioniczen-7709c.cloudfunctions.net/getFeatures?apikey=';
 export const POST_SESSION = '';
 export const POST_ACTION = '';
-export const POST_FEATURE = '';
+export const POST_FEATURE = 'ttps://us-central1-ioniczen-7709c.cloudfunctions.net/postFeature';
 export const POST_FLOW = '';
 
 export interface IDoznConfig {
